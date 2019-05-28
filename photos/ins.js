59801c38b8c87638b5163a038aa82e9d556d5ae2
null <sangehetao@gmail.com>
2019-05-28 20:58:35 +0800

    Site updated: 2019-05-28 20:58:34

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/one-token/PhotosCache/tree/master/min_photos/' + data.link[i];
-          var src = 'https://github.com/one-token/PhotosCache/tree/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/one-token/PhotosCache/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/one-token/PhotosCache/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
